a0a793265e06bd6c45441f17dc916e00abbaa518
arjundevlop <109807311+arjundevlop@users.noreply.github.com>
2023-02-10 15:38:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function randomNumberGenerator() {
 	document.getElementById("num").innerHTML = Math.floor((Math.random() * 20) + 1);
 	var numberGenerated = document.getElementById("num").innerHTML;
 	var guessedValue = document.getElementById("guess").value;
-	if (Math.abs(numberGenerated-guessedValue)==1)
+	if ((Math.abs(numberGenerated-guessedValue)<=5))
 	{
 		document.getElementById("respond").innerHTML = "Hot";
 	}
